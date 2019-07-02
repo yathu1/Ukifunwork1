52bbe242c4a890cf96497b652c02bfd57a9f5e33
yathu1 <yathu2198@gmail.com>
2019-07-02 08:55:17 +0530

    Ukifunwork1

---
+++ main.js
@@ -0,0 +1,19 @@
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function() {
+  SC.stream('/tracks/642673053',function(sound){
+
+       $("#start").click(function(e) {
+              e.preventDefault();
+              sound.start();
+             });
+
+
+              $("#stop").click(function(e) {
+                 e.preventDefault();
+                 sound.stop();
+               });
+});
+});
