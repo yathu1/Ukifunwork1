a3100561a7d6bee8c6c92036fa0feb84b595c44d
yathu1 <yathu2198@gmail.com>
2019-07-05 21:15:31 +0530

    mymusic

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
 $(document).ready(function() {
-  SC.stream('/tracks/568198284',function(sound){
+  SC.stream('/tracks/602050692',function(sound){
 
        $("#start").click(function(e) {
               e.preventDefault();
